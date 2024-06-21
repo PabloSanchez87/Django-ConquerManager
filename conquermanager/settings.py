93b2f72236171823b1804c62b778e84c679d72a1
PabloSanchez <sancheztorrespablo@gmail.com>
2024-06-21 12:06:40 +0200

    Django. Models. Creación del primer modelos. Migraciones

--- conquermanager/settings.py
+++ conquermanager/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [                      ## Aquí indicamos las liberias de terce
     'django.contrib.sessions',          # MAnejo de sesiones
     'django.contrib.messages',          # Manejo de mensajes
     'django.contrib.staticfiles',       # MAnejo de ficheros estáticos.
-    'todos'
+    'todos'                             # Hemos añadido nuestra app/model aquí, sino Django no se entera.
 ]
 
 MIDDLEWARE = [                                              ## Capas por las que pasa una request. Métodos de seg, auth,..
--- todos/models.py
+++ todos/models.py
@@ -3,3 +3,39 @@ from django.db import models
 ## Los modelos es donde vamos a definir nuestras entidades de la app.
 
 # Create your models here.
+
+
+# https://docs.djangoproject.com/es/5.0/topics/db/models/
+class Person(models.Model):
+    first_name = models.CharField(
+        verbose_name="Nombre",
+        max_length=30
+        )
+    last_name = models.CharField(
+        "Apellidos",       # Si es el primer campo ya coge por defecto el valor para Verbose_name.
+        max_length=30,
+        )
+    age = models.IntegerField(
+        "Edad",
+        default=18,
+        help_text="Introduce tu edad en formato número."
+        )
+    dni = models.CharField(
+        "DNI",
+        max_length=9,
+        unique=True
+        )
+
+""" 
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+
+class Album(models.Model):                                          ## Referencia al model tipo Musician.
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)  ## Le indicamos que si se borra el Musician, se borran los artistas.
+    name = models.CharField(max_length=100)                         
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+"""
